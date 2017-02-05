a1f31e791eb042f6a80f7f7dc3aae2505fd40162
Kean Sum Ooi <keansum@gmail.com>
2017-02-05 13:18:05 +0800

    made xmm_t and rte_xmm_t the right size of 16 bytes

--- dpdkNimInc/rte_vect.nim
+++ dpdkNimInc/rte_vect.nim
@@ -3,17 +3,14 @@ const
   XMM_MASK = 15
 
 type
-  xmm_tt* = object #{.union.}
+  xmm_t = object
     b*: array[16, char]
 
-  m128i* = clonglong
-
-  xmm_t* = m128i
-  rte_xmm_t* {.importc: "struct rte_xmm_t", header: "rte_vect.h", union.} = object
+  rte_xmm_t* {.importc: "union rte_xmm", header: "rte_vect.h", union.} = object
     x*: xmm_t
-    u8*: array[(sizeof((xmm_t))) div sizeof((uint8)), uint8]
-    u16*: array[(sizeof((xmm_t))) div sizeof((uint16)), uint16]
-    u32*: array[(sizeof((xmm_t))) div sizeof((uint32)), uint32]
-    u64*: array[(sizeof((xmm_t))) div sizeof((uint64)), uint64]
-    pd*: array[(sizeof((xmm_t))) div sizeof((cdouble)), cdouble]
+    u8*:  array[ XMM_SIZE div sizeof((uint8)), uint8]
+    u16*: array[ XMM_SIZE div sizeof((uint16)), uint16]
+    u32*: array[ XMM_SIZE div sizeof((uint32)), uint32]
+    u64*: array[ XMM_SIZE div sizeof((uint64)), uint64]
+    pd*:  array[ XMM_SIZE div sizeof((cdouble)), cdouble]
 
